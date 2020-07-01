71f014c607b5d4dea145ce5580943c7dc2970bc3
fajernbq <66422481+fajernbq@users.noreply.github.com>
2020-07-01 21:16:07 +0300

    cw6

---
+++ CW6/app/src/main/java/com/example/cw6/Movie.java
@@ -0,0 +1,10 @@
+package com.example.cw6;
+
+public class Movie {
+
+    private String title;
+    private String mainActor;
+    private double movieRate;
+    private int pgRate;
+    private String genre;
+}
